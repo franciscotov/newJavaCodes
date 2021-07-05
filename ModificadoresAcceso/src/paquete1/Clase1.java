c6d4315c5e90f34f892a3d62f3cf04c33b5c6f5a
franciscotov <frank.ronaldo_17@hotmail.com>
2021-07-05 17:56:29 -0300

    Aritmetica

---
+++ ModificadoresAcceso/src/paquete1/Clase1.java
@@ -0,0 +1,13 @@
+
+package paquete1;
+/*
+las clases publicas las podemos acceder desde cualquier
+paquete dentro del proyecto
+*/
+public class Clase1 {
+    public String attrPublico = "Attr Publico";
+    
+    public  Clase1(){
+        System.out.println("Constructor publico");
+    }
+}
